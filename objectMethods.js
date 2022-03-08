815fc73346e466e9df7c031f05acab5ca5a50f2b
hritikdangi3 <hritikdangi3@gmail.com>
2022-03-08 23:47:44 +0530

    minor

---
+++ objectMethods.js
@@ -0,0 +1,25 @@
+
+//given arrays of object and we have to use different methods to work 
+
+METHODS:-
+// Object.keys()  O(n)
+// Object.values();   - O(n)
+// Object.entries();-   O(n)
+// Object.hasOwnProperty(); -   (1)
+
+let array = {
+  firstName: "Hritik",
+  power: "3000",
+  course: "BCA",
+  rank: "100",
+  isStudent: true,
+  favNo: [1, 2, 3],
+};
+Object.values(array);
+
+
+
+
+
+
+
