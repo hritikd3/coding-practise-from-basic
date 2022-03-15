cce0679da8922724d02648fa481c6bbd92a2b39f
hritikdangi3 <hritikdangi3@gmail.com>
2022-03-16 00:26:18 +0530

    refactored same.js

---
+++ refactoredSame.js
@@ -0,0 +1,6 @@
+//write a function called same which accepts two array. the function should return true if every value in array has its corresponding value squared in th second array
+
+// for better approach we will solve it now using frequencyCounter
+
+
+same([1, 2, 3, 2], [9, 4, 4, 1]);
--- same.js
+++ same.js
@@ -1,17 +1,32 @@
 //write a function called same which accepts two array. the function should return true if every value in array has its corresponding value squared in th second array
-
+// tio,e complexity O(n^2)
 function same(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    let correctIndex = arr2.indexOf(arr1[i] ** 2);
-    if (correctIndex === -1) {
-      return false;
-    }
-    console.log(arr2); // false bec all element square are not their
-    arr2.splice(correctIndex, 1);
-  }
-  return true;
+ if(arr1.length !== arr2.length){
+     return false;
+ }
+ let frequencyCounter1= {}
+ let frequencyCounter2= {}
+
+ for(let val of arr1){
+     frequencyCounter1[val]= (frequencyCounter1[val]  || 0) +1
+     
+ }
+ for( let val of arr2){
+     frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+ }
+
+ for(let key in frequencyCounter1){
+     if(!(key ** 2 in frequencyCounter2)){
+         return false;
+     } if(frequencyCounter2[key **2 ] != frequencyCounter1[key]){
+         return false;
+     }
+ }
+ 
+ console.log(frequencyCounter1);
+ console.log(frequencyCounter2);
+ return true;
+
 }
-same([1, 2, 3, 2], [9, 1, 4]);
+
+same([1,3,2,2] , [9,4,4,1])
