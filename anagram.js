f8875b7fe7a13544bd800af7e21308d5fdd94f7b
hritikdangi3 <hritikdangi3@gmail.com>
2022-03-16 18:40:22 +0530

    added comments

--- anagram.js
+++ anagram.js
@@ -9,11 +9,13 @@ function checkAnagram(first,second){
     const lookup={};
     for(let i=0; i< first.length; i++){
     let letter= first[i]
+    // check if letter exist so increament , otherwise set it to 1
    lookup[letter] ? lookup[letter] += 1 : lookup[letter]=1;
     }
+    console.log(lookup);
     for(let i=0; i< second.length;i++){
         let letter=second[i];
-
+//can't find the ltter or ltter is zero then its not a anagram
         if(!lookup[letter])
         return false
         else
