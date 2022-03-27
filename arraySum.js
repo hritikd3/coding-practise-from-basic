619d90f5b73568a5f24b76202541f8dadb5b398b
hritikdangi3 <hritikdangi3@gmail.com>
2022-03-27 11:58:57 +0530

    arraySum

--- arraySum.js
+++ arraySum.js
@@ -1,3 +1,4 @@
+// array sum using loop 
 const array = [1, 2, 3, 4,2];
 let sum = 0;
 
@@ -5,3 +6,14 @@ for (let i = 0; i < array.length; i++) {
   sum += array[i];
 }
 console.log(sum)
+
+//array sum using Reduce() method 
+const arr = [1, 2, 3, 4];
+const reducer = (accumulator, curr) => accumulator + curr;
+console.log(arr.reduce(reducer));
+
+//using lodash library , it do has sum method that can easily add number in array
+var lodash = require("lodash");
+var array1 = [3, 6, 1, 5, 8];
+var summ = lodash.sum(array1);
+console.log(summ);
