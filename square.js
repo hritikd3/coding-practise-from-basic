792f74e342cba2f9d2cefbb735989cec22f446bd
hritikdangi3 <hritikdangi3@gmail.com>
2022-03-30 17:46:59 +0530

    square

---
+++ square.js
@@ -0,0 +1,8 @@
+// Write a program to find the square of given number from 0 too n
+
+function square(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(`square of ${i} is ${i * i}`);
+  }
+}
+square(5);
