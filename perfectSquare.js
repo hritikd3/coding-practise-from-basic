6871abbeefe530fa1f96f0ac451d87096fba15d3
hritikd3 <hritik@intelliapt.com>
2022-04-10 23:36:52 +0530

    added perfectSquare

---
+++ perfectSquare.js
@@ -0,0 +1,8 @@
+// Write a function which returns True if n is a perfect square else False
+// Sample Input 1
+// 4
+// Sample Output 1
+// true
+// Explanation of Sample 1
+// Square root of 4 is 2, hence 4 is a perfect square
+ 
