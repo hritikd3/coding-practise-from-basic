2c7e050971d7086999f6287e99f5e070813d43a8
hritikd3 <hritik@intelliapt.com>
2022-04-10 23:42:32 +0530

    updating refactored

---
+++ perfectSquareRefactored.js
@@ -0,0 +1,17 @@
+// Write a function which returns True if n is a perfect square else False
+// Sample Input 1
+// 4
+// Sample Output 1
+// true
+
+
+// efficient approach using binary search:-
+
+function perfectSquare(n){
+    let low=0, high=n;
+
+    while(low <= high){
+        let mid= parseInt(low+high)/2;
+        if(mid*mid)
+    }
+}
