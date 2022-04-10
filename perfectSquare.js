ec9e8982085ca07377da373ed77ce4f2ecd2ca0e
hritikd3 <hritik@intelliapt.com>
2022-04-10 23:38:47 +0530

    perfectSquare Naiva approach

--- perfectSquare.js
+++ perfectSquare.js
@@ -6,3 +6,19 @@
 // Explanation of Sample 1
 // Square root of 4 is 2, hence 4 is a perfect square
  
+
+
+// Naive approach:-  
+
+function checkPerfect(n){
+    // ceil function computes the nearest greater value 
+if(Math.ceil(Math.sqrt(n))==Math.floor(Math.sqrt(n)))
+{
+	console.log("true")
+}
+else {
+	console.log("false")
+}
+}
+let n=4;
+checkPerfect(n)
